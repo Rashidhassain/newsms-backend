3491f6cfef5b3eb645d8d674315288e979564086
Champ4618 <bhku1997@gmail.com>
2020-02-27 16:34:22 +0530

    rashid

---
+++ app/controller/controller.js
@@ -0,0 +1,86 @@
+const db = require('../config/db.config.js');
+const config = require('../config/config.js');
+const User = db.user;
+const Role = db.role;
+
+var fs = require('fs');
+const Op = db.Sequelize.Op;
+const foreign= db.foreign;
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+
+
+
+
+
+ exports.signup = (req, res) => {
+ 	// Save User to Database
+ 	console.log("Processing func -> SignUp");
+
+ 	User.create({
+
+		username: req.body.username,
+ 		email: req.body.email,
+ 		phone: req.body.phone,
+ 		password: bcrypt.hashSync(req.body.password, 8)
+ 	}).then(user => {
+   Role.findAll({
+ 			where: {
+ 				name: {
+ 					[Op.or]: req.body.roles
+ 				}
+ 			}
+ 		}).then(roles => {
+			user.setRoles(roles).then(() => {
+ 				res.send("User registered successfully!");
+			});
+		}).catch(err => {
+			res.status(500).send("Error -> " + err);
+ 		});
+ 	}).catch(err => {
+		res.status(500).send("Fail! Error -> " + err);
+ 	})
+ }
+
+
+
+
+
+
+
+
+
+exports.signin = (req, res) => {
+	console.log("Sign-In");
+
+	User.findOne({
+		where: {
+			username: req.body.username
+		}
+	}).then(user => {
+		if (!user) {
+			return res.status(404).send('User Not Found.');
+		}
+
+		var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+		if (!passwordIsValid) {
+			return res.status(401).send({ auth: false, accessToken: null, reason: "Invalid Password!" });
+		}
+
+		var token = jwt.sign({ id: user.id }, config.secret, {
+			expiresIn: 86400 // expires in 24 hours
+		});
+
+		res.status(200).send({ auth: true, accessToken: token });
+
+	}).catch(err => {
+		res.status(500).send('Error -> ' + err);
+	});
+}
+
+
+
+exports.get = (req, res) => {
+res.send("hj");
+
+}
